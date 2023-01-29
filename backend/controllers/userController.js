99ff6ebc92f55e034ca6ef5bb1f262a5ac7e77dc
denczik228 <bernikovden@gmail.com>
2023-01-29 07:18:11 +0200

    test of get user data route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -115,7 +115,27 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
+//logout of user
+
+const logout = asyncHandler(async (req, res) => {
+   res.cookie("token", "", {
+     path: "/",
+     httpOnly: true,
+     expires: new Date(0), //1 day
+     sameSite: "none",
+     secure: true,
+   });
+  return res.status(200).json({message:"Successfully logged out"})
+});
+
+//get user data
+const getUser = asyncHandler(async (req, res) => {
+  res.send("get user data")
+  });
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logout,
+    getUser,
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController.js");
+const { registerUser, loginUser, logout, getUser} = require("../controllers/userController.js");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/getuser", getUser);
 
 module.exports = router;
