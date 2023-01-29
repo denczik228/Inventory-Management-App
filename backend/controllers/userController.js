0c12a3053dde5e0576e3284d9131599d29a51de2
denczik228 <bernikovden@gmail.com>
2023-01-29 06:08:36 +0200

    creating of token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,49 +1,63 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 const registerUser = asyncHandler(
     async (req, res) => {
-        const { name, email, password } = req.body;
+      const { name, email, password } = req.body;
 
-        //validation
-        if (!name || !email || !password) {
-            res.status(400);
-            throw new Error("Please fill all required fields");
-        }
-        if (password.length <= 6) {
-            res.status(400);
-            throw new Error("Password less then 6 characters");
-        }
-        //cheking if user email already exist
-        const userExist = await User.findOne({ email });
-        if (userExist) {
-            res.status(400);
-            throw new Error("Email has already exist");
-        }
+      //validation
+      if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please fill all required fields");
+      }
+      if (password.length <= 6) {
+        res.status(400);
+        throw new Error("Password less then 6 characters");
+      }
+      //cheking if user email already exist
+      const userExist = await User.findOne({ email });
+      if (userExist) {
+        res.status(400);
+        throw new Error("Email has already exist");
+      }
 
-       
-        //creating of a new user
-        const user = await User.create({
-            name,
-            email,
-            password,
-        })
-        if (user) {
-            const { _id, name, email, bio, photo, phone } = user;
-            res.status(201).json({
-                _id, name, email, bio, photo, phone
-            })
-        } else {
-            res.status(400);
-            throw new Error("Invalid user data")
-        }
-    
-    if (!req.body.email) {
-        res.status(400)
-        throw new Error("Please add an email")
-    }
-    res.send("Register User")
-});
+      //creating of a new user
+      const user = await User.create({
+        name,
+        email,
+        password,
+      });
+
+      //generate token
+      const token = generateToken(user._id);
+
+      if (user) {
+        const { _id, name, email, bio, photo, phone } = user;
+        res.status(201).json({
+          _id,
+          name,
+          email,
+          bio,
+          photo,
+            phone,
+          token,
+        });
+      } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+      }
+
+      if (!req.body.email) {
+        res.status(400);
+        throw new Error("Please add an email");
+      }
+      res.send("Register User");
+    });
 
 module.exports = {
     registerUser,
